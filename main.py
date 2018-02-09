eb017501f6bf693748bab14794a4453e40b23a31
famishedrover <mudit.verma2014@gmail.com>
2018-02-09 23:51:07 +0530

    commit

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ def info():
 
 
 if __name__ == "__main__":
-	app.run(host = '0.0.0.0' , port=8000)
+	app.run(host = '0.0.0.0' , port=80)
--- pull.sh
+++ pull.sh
@@ -1,1 +1,2 @@
 git pull https://github.com/famishedrover/wildcards_website
+sudo python main.py
